dfdbfbd331e1e22d93104d19730ff895116e0199
Novotnde <55944289+Novotnde@users.noreply.github.com>
2022-07-13 13:02:04 +0200

    Add files via upload

---
+++ BinaryGapProject/BinaryGapProject/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BinaryGapProject 
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number to count its largest binary gap.");
+            var binaryRepre = Console.ReadLine();
+            if(binaryRepre != null)
+            {
+                Console.WriteLine(ComputeLargestBinaryGap(binaryRepre.ToString()));
+            }
+            else
+            {
+                Console.WriteLine("Over");
+            }
+        }
+
+        private static int ComputeLargestBinaryGap(string number)
+        {
+
+            int count = 0;
+            int countMax = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+
+                if (number[i] == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                    countMax = Math.Max(countMax, count);
+                    count = 0;
+                }
+            }
+
+            return countMax;
+        }
+    }
+}
