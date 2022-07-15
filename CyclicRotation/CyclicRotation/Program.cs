724a8a0566f51e3eaf2bc6c6f13cfacb6e2c0bb6
Novotnde <55944289+Novotnde@users.noreply.github.com>
2022-07-15 14:47:17 +0200

    Add files via upload

---
+++ CyclicRotation/CyclicRotation/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CyclicRotation
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World lets moves some elemets in arrays up!");
+            int[] items = new[] { 3, 8, 9, 7, 6 };
+            int rounds = 3;
+            Solution(items, rounds);
+        }
+
+        public static void Solution(int[] A, int K)
+        {
+            var lenght = A.Length;
+            for (int i = 0; i < K; i++)
+            {
+                int numToMove = A[lenght - 1];
+                int newIndex = 0;
+                int oldIndex = A.Length - 1;
+               
+                Array.Copy(A, 0, A, newIndex + 1, oldIndex - newIndex);
+                A[0] = numToMove;
+            }
+        }
+    }
+}
