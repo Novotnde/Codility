2b1367404a808f4f1a097c76ea135ed9f7794a50
Novotnde <55944289+Novotnde@users.noreply.github.com>
2022-07-27 15:42:42 +0200

    Add files via upload

---
+++ OddOccurrencesInArray/OddOccurrencesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OddOccurrencesInArray 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 2,9, 2,2,2 };
+
+       
+            if(nums.Length <= 1)
+            {
+                Console.WriteLine("0");
+            }
+            else if(nums.Length == 1)
+            {
+                Console.WriteLine(nums[0]);
+            }
+            else
+            {
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    var num = nums.Count(s => s == nums[i]);
+
+                    if (num == 1)
+                    {
+                        Console.WriteLine(nums[i]);
+
+                    }
+                }
+            }
+          
+        }
+    }
+
+}
