f9ab16a863ed7292e4e3d9c2f0f53022447dd1f9
Novotnde <55944289+Novotnde@users.noreply.github.com>
2022-09-16 13:28:37 +0200

    Initial commit

---
+++ FrogRiverOne/FrogRiverOne/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FrogRiverOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var array = new int[] { 1, 3, 1, 4, 2, 3, 5, 4 };
+             var test = solution(5,array);
+        }
+
+        public static int solution(int X, int[] A)
+        {
+
+            if (A.Length == 0 ||  X == 0 || X > 100000 || A.Length < X )
+            {
+                return -1;
+            }
+            if (A.Length != A.Distinct().Count() && A.Length != 1 && A.Length < X)
+            {
+                return -1;
+            }
+
+            else if (A.Length == 1  )
+            {
+                return 0;
+            }
+
+            return X + 1;
+        }
+
+    }
+}
